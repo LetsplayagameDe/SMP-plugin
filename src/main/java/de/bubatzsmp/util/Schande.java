a798c7071bbe5b5d88f91169469e8cb1fd52e517
Honey-Craft <shop@honey-craft.net>
2022-01-18 22:39:28 +0100

    18.1.2022

--- src/main/java/de/bubatzsmp/util/Schande.java
+++ src/main/java/de/bubatzsmp/util/Schande.java
@@ -9,14 +9,24 @@ import org.bukkit.entity.Player;
 public class Schande {
 
     public static void sendSchande(Player player, Player target) {
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', String.format(Bubatz.getBubatz().getConfig().getString("message.schande"), target.getPlayer().getDisplayName())));
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', String.format(Bubatz.getBubatz().getConfig().getString("message.schande"), target.getPlayer().getName())));
         target.playSound(target.getLocation(), Sound.ENTITY_CREEPER_PRIMED, 5, 5);
         player.playSound(player.getLocation(), Sound.ENTITY_WITCH_CELEBRATE, 5, 5);
+        if (Bubatz.getBubatz().getConfig().get("Schande." + target.getUniqueId()) == null) {
+            Bubatz.getBubatz().getConfig().set("Schande." + target.getUniqueId().toString(), 0);
+        }
+        long schande = Bubatz.getBubatz().getConfig().getLong("Schande." + target.getUniqueId());
+        Bubatz.getBubatz().getConfig().set("Schande." + target.getUniqueId().toString(), schande + 1);
     }
 
     public static void sendSchandeFromServer(Player target) {
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', String.format(Bubatz.getBubatz().getConfig().getString("message.schande"), target.getPlayer().getDisplayName())));
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', String.format(Bubatz.getBubatz().getConfig().getString("message.schande"), target.getPlayer().getName())));
         target.playSound(target.getLocation(), Sound.ENTITY_CREEPER_PRIMED, 5, 5);
+        if (Bubatz.getBubatz().getConfig().get("Schande." + target.getUniqueId()) == null) {
+            Bubatz.getBubatz().getConfig().set("Schande." + target.getUniqueId().toString(), 0);
+        }
+        long schande = Bubatz.getBubatz().getConfig().getLong("Schande." + target.getUniqueId());
+        Bubatz.getBubatz().getConfig().set("Schande." + target.getUniqueId().toString(), schande + 1);
     }
 
 }
