a5b356b381a00d43d4505c75cde5afc511b062b7
GalaxyCodeZ <galaxycraftez@gmail.com>
2022-01-20 20:16:43 +0100

    "E.A" Patch 2

--- src/main/java/de/bubatzsmp/commands/BedCommand.java
+++ src/main/java/de/bubatzsmp/commands/BedCommand.java
@@ -12,8 +12,10 @@ public class BedCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if (sender instanceof Player player) {
-            Bukkit.getScheduler().runTaskLater(Bubatz.getBubatz(), () -> player.teleport(player.getBedSpawnLocation()), 100);
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&',Bubatz.getBubatz().getConfig().getString("message.bed")));
+            if (player.getBedSpawnLocation() != null) {
+                Bukkit.getScheduler().runTaskLater(Bubatz.getBubatz(), () -> player.teleport(player.getBedSpawnLocation()), 100);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',Bubatz.getBubatz().getConfig().getString("message.bed")));
+            }
         } else {
             sender.sendMessage("Only players.");
         }
